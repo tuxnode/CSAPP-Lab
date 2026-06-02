3878f0192bd49ded7d7eac48457a93dc83d5efdd
tuxnode <tuxnode@outlook.com>
2026-06-02 21:19:00 +0800

    feat: finish Bomb phase_3 -> switch-case control

--- labs/bomb/support.c
+++ labs/bomb/support.c
@@ -119,6 +119,52 @@ void read_six_num(const char *input_string, int *numbers_array) {
   }
 }
 
+/*
+0000000000400f43 <phase_3>:
+  400f43:	48 83 ec 18          	sub    rsp,0x18
+  400f47:	48 8d 4c 24 0c       	lea    rcx,[rsp+0xc]
+  400f4c:	48 8d 54 24 08       	lea    rdx,[rsp+0x8]
+  400f51:	be cf 25 40 00       	mov    esi,0x4025cf
+  400f56:	b8 00 00 00 00       	mov    eax,0x0
+  400f5b:	e8 90 fc ff ff       	call   400bf0 <__isoc99_sscanf@plt>
+  400f60:	83 f8 01             	cmp    eax,0x1
+  400f63:	7f 05                	jg     400f6a <phase_3+0x27>
+  400f65:	e8 d0 04 00 00       	call   40143a <explode_bomb>
+  400f6a:	83 7c 24 08 07       	cmp    DWORD PTR [rsp+0x8],0x7
+  400f6f:	77 3c                	ja     400fad <phase_3+0x6a>
+  400f71:	8b 44 24 08          	mov    eax,DWORD PTR [rsp+0x8]
+  400f75:	ff 24 c5 70 24 40 00 	jmp    QWORD PTR [rax*8+0x402470]
+  400f7c:	b8 cf 00 00 00       	mov    eax,0xcf
+  400f81:	eb 3b                	jmp    400fbe <phase_3+0x7b>
+  400f83:	b8 c3 02 00 00       	mov    eax,0x2c3
+  400f88:	eb 34                	jmp    400fbe <phase_3+0x7b>
+  400f8a:	b8 00 01 00 00       	mov    eax,0x100
+  400f8f:	eb 2d                	jmp    400fbe <phase_3+0x7b>
+  400f91:	b8 85 01 00 00       	mov    eax,0x185
+  400f96:	eb 26                	jmp    400fbe <phase_3+0x7b>
+  400f98:	b8 ce 00 00 00       	mov    eax,0xce
+  400f9d:	eb 1f                	jmp    400fbe <phase_3+0x7b>
+  400f9f:	b8 aa 02 00 00       	mov    eax,0x2aa
+  400fa4:	eb 18                	jmp    400fbe <phase_3+0x7b>
+  400fa6:	b8 47 01 00 00       	mov    eax,0x147
+  400fab:	eb 11                	jmp    400fbe <phase_3+0x7b>
+  400fad:	e8 88 04 00 00       	call   40143a <explode_bomb>
+  400fb2:	b8 00 00 00 00       	mov    eax,0x0
+  400fb7:	eb 05                	jmp    400fbe <phase_3+0x7b>
+  400fb9:	b8 37 01 00 00       	mov    eax,0x137
+  400fbe:	3b 44 24 0c          	cmp    eax,DWORD PTR [rsp+0xc]
+  400fc2:	74 05                	je     400fc9 <phase_3+0x86>
+  400fc4:	e8 71 04 00 00       	call   40143a <explode_bomb>
+  400fc9:	48 83 c4 18          	add    rsp,0x18
+  400fcd:	c3                   	ret
+*/
+
+void phase_3(const char *input) {
+  /*
+  * 主要的检查逻辑是一个巨大的switch case跳转表
+  */
+}
+
 // 炸弹爆炸
 void explode_bomb() {
   puts("BOOM!!!");
--- labs/bomb/support.h
+++ labs/bomb/support.h
@@ -7,6 +7,7 @@ void sig_handler_impl(int x);
 void initialize_bomb();
 void phase_1(const char *input);
 void phase_2(const char *input);
+void phase_3(const char *input);
 void explode_bomb();
 void read_six_num(const char *, int *);
 
