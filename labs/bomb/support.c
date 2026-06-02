8ed12e8dfc9713130f38a93570ccd4e12438fc20
tuxnode <tuxnode@outlook.com>
2026-06-02 11:59:12 +0800

    feat: finish Bomb phase_1

--- labs/bomb/support.c
+++ labs/bomb/support.c
@@ -1,7 +1,10 @@
 #include "support.h"
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+void (*sig_handler)(int);
 
 /*
 00000000004012a0 <sig_handler>:
@@ -39,4 +42,58 @@ void sig_handler_impl(int x) {
 void initialize_bomb() {
   sig_handler = sig_handler_impl;
   signal(2, sig_handler);
+}
+
+/*
+0000000000400ee0 <phase_1>:
+  400ee0:	48 83 ec 08          	sub    rsp,0x8
+  400ee4:	be 00 24 40 00       	mov    esi,0x402400
+  400ee9:	e8 4a 04 00 00       	call   401338 <strings_not_equal>
+  400eee:	85 c0                	test   eax,eax
+  400ef0:	74 05                	je     400ef7 <phase_1+0x17>
+  400ef2:	e8 43 05 00 00       	call   40143a <explode_bomb>
+  400ef7:	48 83 c4 08          	add    rsp,0x8
+  400efb:	c3                   	ret
+*/
+void phase_1(const char *input) {
+  int cmp = strcmp(input, "Border relations with Canada have never been better.");
+  if (cmp == 0) {
+    explode_bomb();
+  }
+}
+
+/*
+0000000000400efc <phase_2>:
+  400efc:	55                   	push   rbp
+  400efd:	53                   	push   rbx
+  400efe:	48 83 ec 28          	sub    rsp,0x28
+  400f02:	48 89 e6             	mov    rsi,rsp
+  400f05:	e8 52 05 00 00       	call   40145c <read_six_numbers>
+  400f0a:	83 3c 24 01          	cmp    DWORD PTR [rsp],0x1
+  400f0e:	74 20                	je     400f30 <phase_2+0x34>
+  400f10:	e8 25 05 00 00       	call   40143a <explode_bomb>
+  400f15:	eb 19                	jmp    400f30 <phase_2+0x34>
+  400f17:	8b 43 fc             	mov    eax,DWORD PTR [rbx-0x4]
+  400f1a:	01 c0                	add    eax,eax
+  400f1c:	39 03                	cmp    DWORD PTR [rbx],eax
+  400f1e:	74 05                	je     400f25 <phase_2+0x29>
+  400f20:	e8 15 05 00 00       	call   40143a <explode_bomb>
+  400f25:	48 83 c3 04          	add    rbx,0x4
+  400f29:	48 39 eb             	cmp    rbx,rbp
+  400f2c:	75 e9                	jne    400f17 <phase_2+0x1b>
+  400f2e:	eb 0c                	jmp    400f3c <phase_2+0x40>
+  400f30:	48 8d 5c 24 04       	lea    rbx,[rsp+0x4]
+  400f35:	48 8d 6c 24 18       	lea    rbp,[rsp+0x18]
+  400f3a:	eb db                	jmp    400f17 <phase_2+0x1b>
+  400f3c:	48 83 c4 28          	add    rsp,0x28
+  400f40:	5b                   	pop    rbx
+  400f41:	5d                   	pop    rbp
+  400f42:	c3                   	ret
+*/
+void phase_2(const char *input) {
+} 
+
+// 炸弹爆炸
+void explode_bomb() {
+  puts("BOOM!!!");
 }
--- labs/bomb/support.h
+++ labs/bomb/support.h
@@ -3,9 +3,10 @@
 
 #include <signal.h>
 
-void (*sig_handler)(int);
 void sig_handler_impl(int x);
-
 void initialize_bomb();
+void phase_1(const char *input);
+void phase_2(const char *input);
+void explode_bomb();
 
 #endif
